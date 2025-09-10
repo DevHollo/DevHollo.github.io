f25352b0256f6d09a552508e1d67c1e01e8d0ef7
Hollo <127456155+DevHollo@users.noreply.github.com>
2025-09-09 21:24:11 -0600

    Update nophone.global.js

--- !assets/scripts/nophone.global.js
+++ !assets/scripts/nophone.global.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        window.location.href = "./mobile.html";
+        window.location.href = "https://devhollo.github.io/pages/mobile.html";
     }
 }
